5e57f1e67b034b582b358288609be6f6959ae5dd
Henry Manes <henrymanes@gmail.com>
2025-06-13 16:40:23 -0700

    Update CORS configuration: Added PUT method to Access-Control-Allow-Methods header in index.js to support additional HTTP requests for enhanced API functionality.

--- functions/index.js
+++ functions/index.js
@@ -91,7 +91,7 @@ app.use((req, res, next) => {
     }
   }
 
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
 
