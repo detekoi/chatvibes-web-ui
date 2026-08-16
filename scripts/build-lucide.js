f2dc917b7826577bcb4babd78db9fa69f086d3d7
Henry Manes <henrymanes@gmail.com>
2026-08-16 13:23:08 -0700

    feat: add additional lucide icons and custom youtube icon to build scripts

--- scripts/build-lucide.js
+++ scripts/build-lucide.js
@@ -24,12 +24,23 @@ import {
   Mic,
   Headphones,
   AlertTriangle,
+  TriangleAlert,
+  CircleHelp,
+  HelpCircle,
+  Book,
+  Settings,
+  ExternalLink,
   ArrowLeft,
   Globe,
   Sparkles,
   ArrowRight,
   LogIn
 } from 'lucide';
+
+const Youtube = [
+  ['path', { d: 'M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17' }],
+  ['path', { d: 'm10 15 5-3-5-3z' }]
+];
 
 window.lucide = {
   createIcons: (options = {}) => {
@@ -47,11 +58,18 @@ window.lucide = {
         Mic,
         Headphones,
         AlertTriangle,
+        TriangleAlert,
+        CircleHelp,
+        HelpCircle,
+        Book,
+        Settings,
+        ExternalLink,
         ArrowLeft,
         Globe,
         Sparkles,
         ArrowRight,
-        LogIn
+        LogIn,
+        Youtube
       },
       ...options
     });
